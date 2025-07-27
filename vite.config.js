1e22884c3cae7b02edfab33ed8a26a59917d896b
Prasadkotkar <prasadkotkar.pk27@gmail.com>
2025-07-27 21:04:31 +0530

    install tailwind.css

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import tailwindScrollbar from 'tailwind-scrollbar';
+
+// https://vite.dev/config/
+export default defineConfig({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      fontfamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [react(),tailwindcss(),/* tailwindScrollbar() */],
+})
